b67f695bdc84207c33ff4e60c5c8e397ba603ba8
Daniel Cabral <daniel.delso@yahoo.com.br>
2022-06-15 11:41:02 -0300

    S3-11 Variaveis js

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = "Daniel";
+let sobreNome = "Cabral";
+let anoNascimento = 1977;
+let anoAtual = 2022
+let cidade = "Recife-PE"
+let idade = anoAtual-anoNascimento;
+console.log(`"Eu sou ${nome + " " + sobreNome} tenho ${idade} anos e moro em ${cidade}."`)
